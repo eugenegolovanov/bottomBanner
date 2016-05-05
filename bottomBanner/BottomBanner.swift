cd021aedc94c4bd39c691bade08023cb373c131b
eugene golovanov <eugene.golovanov@hart.com>
2016-05-04 17:36:25 -0700

    first commit

---
+++ bottomBanner/BottomBanner.swift
@@ -0,0 +1,21 @@
+//
+//  BottomBanner.swift
+//  bottomBanner
+//
+//  Created by eugene golovanov on 5/4/16.
+//  Copyright © 2016 eugene golovanov. All rights reserved.
+//
+
+import UIKit
+
+class BottomBanner: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
